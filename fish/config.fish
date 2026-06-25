e54716dcdbe1a7de956cddfb5b5769203d16b0d5
hiroki706 <hiroki.okubo.tokyo@gmail.com>
2026-06-25 23:31:35 +0900

    a

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,6 @@ set fish_greeting
 abbr -a lg lazygit
 abbr -a oc opencode
 alias mr='mise r'
+
+# Windowsのデフォルトブラウザで開くための設定
+set -x BROWSER '/mnt/c/Windows/System32/rundll32.exe url.dll,FileProtocolHandler'
